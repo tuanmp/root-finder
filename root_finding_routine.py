91551d569b6b2794c67775a65dfbd2f9e59e3ee3
TMPham <tuanpham1503@tgmail.com>
2019-05-22 02:54:33 +0700

    fixed

--- root_finding_routine.py
+++ root_finding_routine.py
@@ -1,22 +1,23 @@
 import math
 
-def root_finder(function, precision):
+def root_finder(function,precision):
 	print("Make a guess of the surrounding values")
-  	upper = input("Upper guess: ")
-  	lower = input("Lower guess: ")
-  
-  	While function(upper)*function(lower) > 0:
-    	print("Please make a different guess")
-    	upper = input("Upper guess: ")
-    	lower = input("Lower guess: ")
-    
-  	x=(lower+upper)/2.0
-  	While abs(function(x)) > precision:
-    		If function(x)>0:
-      			upper = x
-      			x = (lower+upper)/2.0
-    		Else: 
+	upper = float(input("Upper guess: "))
+	lower = float(input("Lower guess: "))
+	print (type(upper))
+	print (function(upper))
+	#
+	while function(upper)*function(lower) > 0:
+		print("Please make a different guess")
+		upper = float(input("Upper guess: "))
+		lower = float(input("Lower guess: "))
+
+	x=(lower+upper)/2.0
+	while abs(function(x)) > precision:
+		if f(x)>0:
+			upper = x
+			x = (lower+upper)/2.0
+		else:
 			lower=x
 			x=(lower+upper)/2.0
-	Return(x)
-		
+	return(x)

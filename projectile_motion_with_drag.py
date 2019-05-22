59586801036cdcf031df57bcfbf0939ccfc1f58c
TMPham <tuanpham1503@tgmail.com>
2019-05-22 11:03:10 +0700

    Update projectile_motion_with_drag.py

--- projectile_motion_with_drag.py
+++ projectile_motion_with_drag.py
@@ -1,9 +1,12 @@
 import math
 import root_finding_routine as rf
+import numpy as np
+import matplotlib.pyplot as plt
 
 g = 9.8
 theta_0 = float(input("Launch angle(degrees) : "))
 launch_angle = math.radians(theta_0)
+print(launch_angle)
 mass = float(input("Projectile mass(kg): "))
 b = float(input("Drag coefficient(kg.s^-2): "))
 v_0 = float(input("Initial spped: "))
@@ -12,9 +15,8 @@ v_ter = mass*g/b
 u = v_0/v_ter
 
 
-def function(x):
-    y = x*u**2*math.tan(launch_angle) + x*u/(math.cos(launch_angle))
-    + math.log(1 - x*u/(math.cos(launch_angle)), math.e)
+def function(t):
+    y = t*u**2*np.tan(launch_angle) + t*u/(np.cos(launch_angle)) + np.log(1 - t*u/(np.cos(launch_angle)))
     return y
 
 

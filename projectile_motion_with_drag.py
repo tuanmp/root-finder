58355eaf9a982cc4089c1127db0d4cab1cf3183b
TMPham <tuanpham1503@tgmail.com>
2019-05-22 09:47:01 +0700

    projectile

---
+++ projectile_motion_with_drag.py
@@ -0,0 +1,25 @@
+import math
+import root_finding_routine as rf
+
+g = 9.8
+theta_0 = float(input("Launch angle(degrees) : "))
+launch_angle = math.radians(theta_0)
+mass = float(input("Projectile mass(kg): "))
+b = float(input("Drag coefficient(kg.s^-2): "))
+v_0 = float(input("Initial spped: "))
+r_max = v_0**2/g
+v_ter = mass*g/b
+u = v_0/v_ter
+
+
+def function(x):
+    y = x*u**2*math.tan(launch_angle) + x*u/(math.cos(launch_angle))
+    + math.log(1 - x*u/(math.cos(launch_angle)), math.e)
+    return y
+
+
+x = rf.root_finder(function, 0.00000001)
+
+range = x*r_max
+
+print(range)
